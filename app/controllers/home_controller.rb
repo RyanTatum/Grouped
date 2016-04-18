7f80cbe239a68103924ce9c2804eca1f366d8485
dnham <blake-dunham@uiowa.edu>
2016-04-18 04:27:27 +0000

    logging in no longer takes you to blank screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     before_filter :set_current_user
     
     def index
-        
+        redirect_to user_path(session[:current_user]["objectId"])
     end
     
     def show 
