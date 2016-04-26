0b7e268baa0101e4447c281c516fc20e03d33651
aclendenen <alex-clendenen@uiowa.edu>
2016-04-26 00:30:49 +0000

    variable cols

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -24,16 +24,19 @@ class SprintsController < ApplicationController
             j.eq("objectId", session[:current_user]["current_groupId"])
         end.get.first
         @diff_size = @group["star_count"];
-        #@columns = @group["sprint_names"];
+        @columns = @group["sprint_names"];
+        if(@columns == nil || @columns == "" || @columns == [])
+           @columns = ["Todo", "Doing", "Done"] 
+        end
         #@column_size = @columns.length;
         @users = @client.query("User_Group").tap do |j|
             j.eq("group_ptr", Parse::Pointer.new({"className" => "Group", "objectId" => session[:current_user]["current_groupId"]}))
             j.include = "user_info_ptr"
         end.get
         
-        @columns = @client.query("Sprint_Columns").tap do |j|
-            j.eq("group_ptr", Parse::Pointer.new({"className" => "Group", "objectId" => session[:current_user]["current_groupId"]}))
-        end.get
+        #@columns = @client.query("Sprint_Columns").tap do |j|
+         #   j.eq("group_ptr", Parse::Pointer.new({"className" => "Group", "objectId" => session[:current_user]["current_groupId"]}))
+        #end.get
         #@sprints = @sprints_query.get
         #@users_query = @client.query("User_Info")
         #@users = @users_query.get
