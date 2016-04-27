f263c963a583d6000ba37da3ebf342470a2a694f
dnham <blake-dunham@uiowa.edu>
2016-04-27 15:53:33 +0000

    Group selections shows up on video chat

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,4 +1,10 @@
 class VideoController < ApplicationController
+    
+    skip_before_action :verify_authenticity_token
+    before_filter :set_current_user
+    before_filter :set_client
+    before_filter :set_cache_headers
+    
     def index 
     end 
 end
